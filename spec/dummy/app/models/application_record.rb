a1ab073373e8cca04d8617d078045b2d4f9203bb
Raza Khan <raza@vinsol.com>
2023-08-09 18:41:52 +0530

    DEVOPS-1462 update ransack to version 4.0.0

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -4,4 +4,14 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   scope :published, -> {}
+
+  class << self
+    def ransackable_attributes(_auth_object = nil)
+      column_names
+    end
+
+    def ransackable_associations(_auth_object = nil)
+      reflect_on_all_associations.map { |association| association.name.to_s }
+    end
+  end
 end
